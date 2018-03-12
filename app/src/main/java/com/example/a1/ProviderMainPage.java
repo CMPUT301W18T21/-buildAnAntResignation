fc4031a83e4fd890c2ccb8593fee02778fee5dd2
TiannanXu <AlexXu46871@users.noreply.github.com>
2018-03-11 22:34:34 -0600

    Add provider main page
    activity and class has been created

---
+++ app/src/main/java/com/example/a1/ProviderMainPage.java
@@ -0,0 +1,8 @@
+package com.example.a1;
+
+/**
+ * Created by remil on 2018/3/11.
+ */
+
+public class ProviderMainPage {
+}
---
+++ app/src/main/java/com/example/a1/ProviderMainPageView.java
@@ -0,0 +1,13 @@
+package com.example.a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProviderMainPageView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_main_page_view);
+    }
+}
