2ca1fb0e2ffff71d7cf294834f78b369bd6c93be
Yuant9 <yt9@ualberta.ca>
2018-03-19 11:06:52 -0600

    three people's branches merged

---
+++ app/src/main/java/com/example/a1/pmpItemClickListener.java
@@ -0,0 +1,15 @@
+package com.example.a1;
+
+import android.view.View;
+
+
+
+/**
+ * Created by Alex on 2018-03-19.
+ */
+
+public interface pmpItemClickListener {
+
+    void onItemClick(View v);
+}
+
