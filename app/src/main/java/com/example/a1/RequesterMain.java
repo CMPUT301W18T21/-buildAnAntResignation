cb1e6210e224e6e5f06a7238cb6845859c5fc442
Nik Teterski <nik@teter.ski>
2018-03-24 15:02:31 -0600

    Now sending selected task to Requester Task Details screen.

--- app/src/main/java/com/example/a1/RequesterMain.java
+++ app/src/main/java/com/example/a1/RequesterMain.java
@@ -85,6 +85,17 @@ public class RequesterMain extends AppCompatActivity {
         setupRequestView();
         setupAssignedButton();
 
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                        Intent intent = new Intent(RequesterMain.this, RequesterTaskDetail.class);
+                        Task task = user.getRequestedTask((int)id);
+                        intent.putExtra("task", task);
+                        startActivityForResult(intent, 1);
+                    }
+                }
+        );
+
     }
 
     /**
