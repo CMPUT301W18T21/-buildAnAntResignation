dc8d993f2a4c1a9ebde7a90da6f85fb72b7cc4da
TiannanXu <32802742+TiannanXu@users.noreply.github.com>
2018-03-28 14:48:26 -0600

    fix pmp jump issue

--- app/src/main/java/com/example/a1/ProviderMainPage.java
+++ app/src/main/java/com/example/a1/ProviderMainPage.java
@@ -1,13 +1,20 @@
 package com.example.a1;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -35,7 +42,7 @@ public class ProviderMainPage extends AppCompatActivity {
         setContentView(R.layout.activity_provider_main_page);
         showContent = (ListView) findViewById(R.id.listView);
         keyWords = (SearchView) findViewById(R.id.searchView);
-        final pmpCustomAdapter customAdapter = new pmpCustomAdapter(this,boundinfo());
+        final CustomAdapter customAdapter = new CustomAdapter(this,boundinfo());
         showContent.setAdapter(customAdapter);
 
 
@@ -100,6 +107,135 @@ public class ProviderMainPage extends AppCompatActivity {
         /* this part should handle google map api connection */
 
     }
+    class CustomAdapter extends BaseAdapter implements Filterable {
+
+        Context c;
+        ArrayList<ProviderAdaptInfo> providerinfos;
+        LayoutInflater inflater;
+        CustomFilter filter;
+        ArrayList<ProviderAdaptInfo> filterList;
+
+        //custom adapter constructor
+
+        public CustomAdapter(Context ctx, ArrayList<ProviderAdaptInfo> providerinfos) {
+            this.c = ctx;
+            this.providerinfos = providerinfos;
+            this.filterList = providerinfos;
+        }
+
+        @Override
+        public int getCount() {
+            return providerinfos.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return providerinfos.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(final int pos, View convertView, ViewGroup parent) {
+
+            if (inflater == null) {
+                inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            }
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.custom_pp, null);
+
+            }
+
+
+
+            pmpViewHolder holder = new pmpViewHolder(convertView);
+
+
+            holder.textView_task.setText(providerinfos.get(pos).getTask());
+            holder.textView_username.setText(providerinfos.get(pos).getName());
+            holder.textView_status.setText(providerinfos.get(pos).getStatus());
+            holder.textView_lowestBid.setText(providerinfos.get(pos).getLowestbid().toString());
+
+
+            holder.setItemClickListener(new pmpItemClickListener() {
+                                            @Override
+                                            public void onItemClick(View view) {
+                                                Intent intent = new Intent(ProviderMainPage.this, ProviderBiddedTask.class);
+                                                startActivity(intent);
+                                                Toast.makeText(c, providerinfos.get(pos).getTask(), Toast.LENGTH_SHORT).show();
+
+
+
+                                            }
+
+                                        }
+
+            );
+
+            return convertView;
+        }
+        @Override
+        public Filter getFilter() {
+
+            if (filter == null)
+            {
+                filter = new CustomFilter();
+            }
+
+            return filter;
+        }
+
+        class CustomFilter extends Filter {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+                FilterResults results = new FilterResults();
+
+                if (constraint != null && constraint.length() > 0) {
+                    constraint = constraint.toString().toLowerCase();
+
+                    ArrayList<ProviderAdaptInfo> filters = new ArrayList<>();
+
+                    for (int i = 0; i < filterList.size(); i++) {
+                        if (filterList.get(i).getTask().toLowerCase().contains(constraint)) {
+                            ProviderAdaptInfo tempinfo = new ProviderAdaptInfo(filterList.get(i).getName(),
+                                    filterList.get(i).getTask(),
+                                    filterList.get(i).getStatus(),
+                                    filterList.get(i).getLowestbid());
+                            filters.add(tempinfo);
+                        }
+                    }
+
+                    results.count = filters.size();
+                    results.values = filters;
+
+                } else {
+
+                    results.count = filterList.size();
+                    results.values = filterList;
+                }
+
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                providerinfos = (ArrayList<ProviderAdaptInfo>) results.values;
+                notifyDataSetChanged();
+
+            }
+        }
+
+
+    }
+
 
 
 }
