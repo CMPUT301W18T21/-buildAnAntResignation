3fc72be682d31566d8bebabfa7d2bec8c2705ba7
Nik Teterski <nik@teter.ski>
2018-04-06 14:28:01 -0600

    More minor UI changes: Added proper titles to most Requester screens, and to login,main, and contact info screens.

--- app/src/main/java/com/example/a1/DialogChangeStatus.java
+++ app/src/main/java/com/example/a1/DialogChangeStatus.java
@@ -16,6 +16,7 @@ public class DialogChangeStatus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_change_status);
+        setTitle("Assigned Task Options");
     }
 
     public static void setTask(Task task){
